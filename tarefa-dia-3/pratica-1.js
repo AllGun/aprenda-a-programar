5554d490a57e7ef06afe3ea6162617e374a01d3c
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-05-19 06:33:36 -0300

    Variáveis criadas no pratica-1.js

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,5 @@
+const name = "Alfredo Gutemberg";
+const email ="alfredo.gutemberg@gmail.com";
+const profissao = "Programador";
+const idade = "36";
+const umLugarParaConhecer = "Shenzhen - China";
