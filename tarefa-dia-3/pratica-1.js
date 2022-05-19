35a52b1b17424e474ab05f33231af08c60e80b02
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-05-19 06:56:16 -0300

    Texto adicionado

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -1,7 +1,9 @@
 const clientName = "Alfredo Gutemberg";
-const email ="alfredo.gutemberg@gmail.com";
-const occupation = "Programador Java";
+const clientEmail ="alfredo.gutemberg@gmail.com";
+const clientOccupation = "Programador Java";
 const clientAge = 36;
 const placeToVisit = "Shenzhen - China";
 
-console.log(`Olá, ${clientName}`)
+console.log(`Olá, ${clientName}!`);
+console.log(`Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${placeToVisit}, uma das cidades mais tecnológicas do mundo.`);
+console.log(`Confirma pra gente alguns dados? A sua idade é ${clientAge}, você é ${clientOccupation} e seu e-mail é ${clientEmail}?\n Ficamos no aguardo. Muito obrigado e boa viagem! :D `);
