53151c51509fba0af0250a8a3e64907431498e7f
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-05-20 00:12:00 -0300

    Prática 4 resolvida

--- tarefa-dia-3/pratica-4.js
+++ tarefa-dia-3/pratica-4.js
@@ -6,24 +6,41 @@ let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
 if(produtoA.internacional) {
     let imposto = produtoA.valor * 0.2;
     console.log(`Imposto: R$${imposto}`);
+    let precoTotal = produtoA.valor + imposto;
+    console.log(`Preço total: R$${precoTotal}`);
+    console.log();
 } else {
     let imposto = produtoA.valor * 0.15;
     console.log(`Imposto: R$${imposto}`);
+    let precoTotal = produtoA.valor + imposto;
+    console.log(`Preço total: R$${precoTotal}`);
+    console.log();
 }
+
 
 if(produtoB.internacional) {
-    let imposto = produtoA.valor * 0.2;
+    let imposto = produtoB.valor * 0.2;
     console.log(`Imposto: R$${imposto}`);
-
+    let precoTotal = produtoB.valor + imposto;
+    console.log(`Preço total: R$${precoTotal}`);
+    console.log();
 } else {
     let imposto = produtoA.valor * 0.15;
     console.log(`Imposto: R$${imposto}`);
+    let precoTotal = produtoC.valor + imposto;
+    console.log(`Preço total: R$${precoTotal}`);
+    console.log();
 }
+
 
 if(produtoC.internacional) {
     let imposto = produtoA.valor * 0.2;
     console.log(`Imposto: R$${imposto}`);
+    let precoTotal = produtoC.valor + imposto;
+    console.log(`Preço total: R$${precoTotal}`);
 } else {
     let imposto = produtoA.valor * 0.15;
     console.log(`Imposto: R$${imposto}`);
+    let precoTotal = produtoC.valor + imposto;
+    console.log(`Preço total: R$${precoTotal}`);
 }
