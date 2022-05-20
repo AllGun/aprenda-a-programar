bfa9e73ccc8a7a975d08905a7b83c018f0582619
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-05-20 00:04:24 -0300

    Prática 3 resolvida

---
+++ tarefa-dia-3/aula-dia-4.js
@@ -0,0 +1,21 @@
+function printName () {
+    console.log("Alfredo Gutemberg");
+}
+
+
+function somar (number1, number2) {
+    console.log(`O primeiro número é ${number1}`);
+    console.log(`O segundo número é ${number2}`);
+    let soma = number1 + number2;
+    console.log(`${number1} + ${number2} = soma`);
+}
+
+somar(10, 10);
+
+let i;
+
+for (i = 0; i < 9; i++) {
+    console.log(i);
+}
+
+console.log(i);
